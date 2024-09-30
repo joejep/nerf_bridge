0c5fa1eef6b46f79b4543554d4c52ba5b7c7fb2c
Ibrahim Dahiru Dahiru <34832399+Daheer@users.noreply.github.com>
2024-09-30 22:02:03 +0000

    feat: fix on ros_export

--- nsros/ros_exporter.py
+++ nsros/ros_exporter.py
@@ -450,27 +450,3 @@ class ExportCameraPoses(Exporter):
                 json.dump(frames, f, indent=4)
 
             CONSOLE.print(f"[bold green]:white_check_mark: Saved poses to {output_file_path}")
-
-
-Commands = tyro.conf.FlagConversionOff[
-    Union[
-        Annotated[ExportPointCloud, tyro.conf.subcommand(name="pointcloud")],
-        Annotated[ExportTSDFMesh, tyro.conf.subcommand(name="tsdf")],
-        Annotated[ExportPoissonMesh, tyro.conf.subcommand(name="poisson")],
-        Annotated[ExportMarchingCubesMesh, tyro.conf.subcommand(name="marching-cubes")],
-        Annotated[ExportCameraPoses, tyro.conf.subcommand(name="cameras")],
-    ]
-]
-
-
-def entrypoint():
-    """Entrypoint for use with pyproject scripts."""
-    tyro.extras.set_accent_color("bright_yellow")
-    tyro.cli(Commands).main()
-
-
-if __name__ == "__main__":
-    entrypoint()
-
-# For sphinx docs
-get_parser_fn = lambda: tyro.extras.get_parser(Commands)  # noqa
