e7a714ac0207e662ddb711c3a8b87ec4ac262f3f
Ibrahim Dahiru Dahiru <34832399+Daheer@users.noreply.github.com>
2024-10-02 11:24:42 +0000

    test

--- nsros/ros_dataloader.py
+++ nsros/ros_dataloader.py
@@ -202,7 +202,7 @@ class ROSDataloader(DataLoader):
         while True:
             if self.updated or self.bypass_updated:
                 self.batch = self._get_updated_batch()
-                self.updated = False if not self.bypass_updated
+                self.updated = False
 
             batch = self.batch
             yield batch
