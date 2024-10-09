adc18642d40ef2f7cb09550ef902d10f2057f4b8
Ibrahim Dahiru Dahiru <34832399+Daheer@users.noreply.github.com>
2024-10-09 06:48:50 +0000

    try

--- nsros/ros_dataloader.py
+++ nsros/ros_dataloader.py
@@ -205,12 +205,15 @@ class ROSDataloader(DataLoader):
     def __iter__(self):
         if self.bypass_updated:
             count = 0
+        count = 0
         while True:
             if self.updated:
                 # if self.bypass_updated: self.current_idx += 1
                 self.batch = self._get_updated_batch()
                 self.updated = False
             batch = self.batch
+            count += 1
+            print(count)
             if self.bypass_updated:
                 if count < 500:
                     count += 1
